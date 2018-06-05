4722292ab0e6dae6560ca1841ae5bd5b8752f93e
LzxHahaha <lzxglhf@live.com>
2018-06-06 00:16:54 +0800

    update

--- ui/src/lib/adb/EventRecord.js
+++ ui/src/lib/adb/EventRecord.js
@@ -1,6 +1,6 @@
 import AdbBase from './AdbBase';
 
-export default class ScreenRecord extends AdbBase {
+export default class EventRecord extends AdbBase {
   constructor(options = {}) {
     super(options);
     this._baseArgs = ['shell', 'getevent', '-t'];
@@ -24,9 +24,26 @@ export default class ScreenRecord extends AdbBase {
       }
     }
     const eventStr = this._events.slice(i);
-    return eventStr.map((el) => {
-      // TODO: 解析
-      return el;
+
+    const events = eventStr.map((el) => {
+      const [, timestamp, device, type, code, value] = /^[]/.match(el);
+      return {
+        timestamp: parseFloat(timestamp),
+        device,
+        type: parseInt(type, 16),
+        code: parseInt(code, 16),
+        value: parseInt(value, 16),
+        next: 0
+      };
     });
+    for (let i = 0; i < events.length - 1; ++i) {
+      events[i].next = events[i + 1].timestamp - events[i].timestamp;
+    }
+
+    return events;
+  }
+
+  execute() {
+
   }
 }
